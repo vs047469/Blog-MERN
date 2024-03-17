5df014d4bd2a5b9e50a60b36e5dd1f92623d47d3
vs047469 <36482424+vs047469@users.noreply.github.com>
2024-03-17 11:22:44 +0530

    Add files via upload
    client commit

---
+++ client/src/Pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="max-w-3xl mx-auto px-4 py-8">
+      <h2 className="text-5xl font-extrabold leading-custom text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500 mb-6">Who We Are</h2>
+      <p className="text-lg text-gray-700 leading-relaxed">
+        Welcome to <span className="font-bold">SinghBlogs</span>, where passion meets prose and creativity thrives. Founded with the belief that everyone has a story to tell, we strive to provide a platform where voices from all walks of life can be heard. Whether you're a budding wordsmith, a seasoned storyteller, or simply curious about the world around you, our community is here to support and inspire you on your journey. Through the power of blogging, we aim to cultivate connections, spark conversations, and celebrate the richness of human experience. Join us as we explore the art of writing, share our passions, and connect with like-minded individuals from around the globe. Together, let's embark on a journey of discovery, expression, and creativity. Welcome to <span className="font-bold">SinghBlogs</span>, where words come alive.
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ client/src/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="max-w-2xl mx-auto px-5 py-8">
+       <h1 className="text-5xl font-extrabold leading-custom text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500 mb-6">Welcome to Our Blogging!</h1>
+       <p className="text-lg text-gray-700 leading-relaxed border border-gray-300 p-4">
+        Thank you for visiting our blog! Here, you'll discover a treasure trove of insightful articles, 
+        captivating stories, and thought-provoking content. Whether you're a seasoned blogger or just starting 
+        out on your writing journey, we're here to inspire and empower you to create exceptional blog posts 
+        that resonate with your audience.
+      </p>
+    </div>
+  )
+}
+
+export default Home
